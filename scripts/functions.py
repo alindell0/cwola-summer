352c9101e098050398c869783c909240468dec4b
Anika Lindell <atlindell@gmail.com>
2026-07-20 16:13:32 -0700

    Reorganization, sim-stream k-fold run, check gpu speed...

--- scripts/functions.py
+++ scripts/functions.py
@@ -3,6 +3,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
+import seaborn as sns
+from torch import nn
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+import copy
+import wandb
+import os
 
 
 
@@ -18,7 +27,9 @@ def load_data(patch_idx):
 
 
 
-def plot_data(df):
+def plot_data(df, save_folder = f'simulated-streams/plots'):
+
+    os.makedirs(save_folder, exist_ok=True)
     
     # create figure with 1 row and 3 columns for data subplots
     fig, axes = plt.subplots(1, 3, figsize=(15, 5), tight_layout=True)
@@ -51,6 +62,7 @@ def plot_data(df):
     c.ax.set_title('Counts', fontsize=8)
 
     plt.show()
+    plt.savefig(os.path.join(save_folder, "alldataplots.png"))
 
     df_stream = df[df['stream']==True]
 
@@ -85,7 +97,7 @@ def plot_data(df):
     c.ax.set_title('Counts', fontsize=8)
 
     plt.show()
-
+    plt.savefig(os.path.join(save_folder, "streamdataplots.png"))
 
 
 # define signal sideband region
@@ -157,7 +169,273 @@ def signal_sideband(df, pm_parameter='rotpmdec', sig_factor=0.25, sb_factor=0.5,
 
 
 
+def cwola_train(df, pm_parameter='rotpmdec', dropout=0.2, k_folds=5, batch_size=10000, lr=0.001, patience=30, epochs=100, trainval_loops=3, save_folder='../results/simulated-streams/sim-1patch', wandbproj='sim-1patch'):
+
+    os.makedirs(save_folder, exist_ok=True)
+
+    # training variables
+    if pm_parameter == 'rotpmdec':
+        training_vars = ['b-r', 'g', 'rotra', 'rotdec', 'rotpmra']
+    if pm_parameter == 'rotpmra':
+        training_vars = ['b-r', 'g', 'rotra', 'rotdec', 'rotpmdec']
+
+    # model architecture
+    class NeuralNetwork(nn.Module):
+        def __init__(self, input_dim = 5, hidden_dim = 256, output_dim = 1, dropout=dropout): # create tool, parameters for recalling, call function is used after
+            super().__init__() # access all methods, attributes, etc of class
+            self.NeuralNetwork = nn.Sequential(
+                nn.Linear(input_dim, hidden_dim),
+                nn.ReLU(),
+                nn.Dropout(p=dropout),       
+                nn.Linear(hidden_dim, hidden_dim),
+                nn.ReLU(),
+                nn.Dropout(p=dropout),       
+                nn.Linear(hidden_dim, hidden_dim),
+                nn.ReLU(),
+                nn.Dropout(p=dropout),       
+                nn.Linear(hidden_dim, output_dim),
+                nn.Sigmoid()
+            )
+        def forward(self, x):
+            return self.NeuralNetwork(x)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'Using device: {device}')
+
+    # k-folding
+    skf = StratifiedKFold(n_splits=k_folds, shuffle=True, random_state=18)
+
+    fold_stars = [] # will be a list of k (5) arrays, representing the k folds
+    for fold_idx, (train_index, test_index) in enumerate(skf.split(df[training_vars], df['region_label'])):
+        fold_stars.append(test_index)
+    fold_labels = np.arange(len(fold_stars)) # fold labels is an array of values 0-4, representing indices/labels of the 5 folds
     
+    test_dfs = []
+    for test_idx in fold_labels:
+        test_stars = fold_stars[test_idx]
+        save_folder_test = os.path.join(save_folder, "test_fold_{}".format(test_idx))
+        os.makedirs(save_folder_test, exist_ok=True)
+
+        # cycles through remaining sets (minus the test set) for validation sets
+        for val_idx in np.delete(fold_labels, test_idx):
+            val_stars = fold_stars[val_idx]
+    
+            train_indices = np.delete(fold_labels, [test_idx, val_idx])
+            train_stars = np.concatenate([fold_stars[train_idx] for train_idx in train_indices])
+    
+            # create data frames with indices
+            df_train = df.iloc[train_stars]
+            df_val = df.iloc[val_stars]
+            df_test = df.iloc[test_stars]
+    
+            train_x = df_train[training_vars]
+            train_y = df_train['region_label'].to_numpy()
+    
+            val_x = df_val[training_vars]
+            val_y = df_val['region_label'].to_numpy()
+    
+            test_x = df_test[training_vars]
+            test_y = df_test['region_label'].to_numpy()
+            test_y_true = df_test['stream'].to_numpy(dtype=int)
+
+            # scale data
+            scaler = StandardScaler()
+            
+            train_x = scaler.fit_transform(train_x) # fit_transform called only on train data
+            val_x = scaler.transform(val_x)
+            test_x = scaler.transform(test_x)
+                    
+
+            ## create tensors, datasets, and loaders
+            train_x_tensor = torch.tensor(np.array(train_x))
+            train_y_tensor = torch.tensor(train_y)
+                
+            val_x_tensor = torch.tensor(np.array(val_x))
+            val_y_tensor = torch.tensor(val_y)
+                
+            test_x_tensor = torch.tensor(np.array(test_x))
+            test_y_tensor = torch.tensor(test_y)
+            test_y_true_tensor = torch.tensor(test_y_true)
+                
+            train_dataset = TensorDataset(train_x_tensor, train_y_tensor)
+            val_dataset = TensorDataset(val_x_tensor, val_y_tensor)
+            test_dataset = TensorDataset(test_x_tensor, test_y_tensor, test_y_true_tensor)
+                
+            train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+            val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+            test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+            best_loop_val_loss = float('inf')
+
+            test_nn_scores = []
+            
+            for n in range(trainval_loops):
+
+                # initialize model
+                run = wandb.init(project=wandbproj, 
+                            group=f"fold_{test_idx}", 
+                            job_type=f"val_set_{val_idx}", 
+                            name=f"test_{test_idx}_val_{val_idx}_train_{n}",
+                            config={"test_fold": test_idx, "val_set": val_idx, "train_loop": n},
+                            reinit=True)
+                model = NeuralNetwork(input_dim = 5, hidden_dim = 256, output_dim = 1, dropout=dropout).to(device)
+                loss_fn = nn.BCELoss()
+                optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    
+                best_val_loss = float('inf')
+                patience_counter = 0
+                
+                for epoch in range(epochs):
+                
+                    # training
+                    model.train()
+                    total_train_loss = 0
+                    train_correct, counts = 0, 0
+                    for inputs, region_labels in train_loader:
+                        optimizer.zero_grad()
+                        inputs = inputs.to(device).float()
+                        region_labels = region_labels.to(device).float()
+                        
+                        out = model(inputs).squeeze(1)
+                        loss = loss_fn(out, region_labels)
+                        loss.backward()
+                        optimizer.step()
+                        total_train_loss += loss.item()
+                
+                        predictions = (out > 0.5).float()
+                        train_correct += (predictions == region_labels).sum().item()
+                        counts += region_labels.size(0)
+                
+                    avg_train_loss = total_train_loss / len(train_loader)
+                    train_accuracy = train_correct / counts
+                
+                    # validation
+                    model.eval()
+                    total_val_loss = 0
+                    val_correct, counts = 0.0, 0
+                    for inputs, region_labels in val_loader:
+                        inputs = inputs.to(device).float()
+                        region_labels = region_labels.to(device).float()
+                
+                        out = model(inputs).squeeze(1)  # forward pass
+                        loss = loss_fn(out, region_labels)  # compute loss
+                        total_val_loss += loss.item()
+                
+                        predictions = (out > 0.5).float()
+                        val_correct += (predictions == region_labels).sum().item()
+                        counts += region_labels.size(0)
+                
+                    avg_val_loss = total_val_loss / len(val_loader)
+                    val_accuracy = val_correct / counts
+
+                    wandb.log({"train_loss": avg_train_loss, "val_loss": avg_val_loss, 
+                             "train_acc": train_accuracy, "val_acc": val_accuracy, "epoch": epoch})
+                    
+                    # early stopping
+                    if avg_val_loss < best_val_loss:
+                        best_val_loss = avg_val_loss
+                        patience_counter = 0
+                        best_model = copy.deepcopy(model.state_dict())
+                    else:
+                        patience_counter += 1
+                
+                    if patience_counter >= patience:
+                        print("Early stopping")
+                        break
+                
+                if best_val_loss < best_loop_val_loss:
+                    best_loop_val_loss = best_val_loss
+                    lowest_val_loss_model = best_model ## model parameters from epoch with the lowest validation loss
+
+                run.finish()
+
+            model_save_path = os.path.join(save_folder_test, "val_set_{}_best_model".format(val_idx))
+            torch.save(lowest_val_loss_model, model_save_path) ## model parameters from training run with the lowest validation loss
+
+            model.load_state_dict(lowest_val_loss_model)
+
+            model.eval()
+            test_loss, test_correct, counts = 0.0, 0.0, 0
+            raw_scores = []
+            
+            with torch.no_grad():
+                for inputs, region_labels, true_labels in test_loader:
+                    
+                    inputs = inputs.to(device).float()
+                    region_labels = region_labels.to(device).float()
+                    out = model(inputs).squeeze()
+                    raw_scores.extend(out.squeeze().cpu().numpy())
+                    loss = loss_fn(out, region_labels)
+                    test_loss += loss.item() * inputs.size(0)
+            
+                    scores = (out > 0.5).float()
+                    test_correct += (scores == region_labels).sum().item()
+                    counts += region_labels.size(0)
+            
+            test_nn_scores.append(raw_scores)
+    
+        df_test['nn_score'] = np.mean(test_nn_scores, axis=0)
+        test_dfs.append(df_test)
+    
+    df_test_full = pd.concat([df for df in test_dfs])
+    df_test_full.to_hdf(f'{save_folder}/df_test.h5', key=f'data', mode='w')
+        
+    return df_test_full
+
+
+def fiducial_cuts(df): # CWOLA Stellar Stream fiducial cuts https://arxiv.org/pdf/2305.03761
+    df = df[df['g'] < 20.2] # ensures uniform acceptance by Gaia satellite
+    df = df[(np.abs(df['rotpmdec']) > 2) | (np.abs(df['rotpmra']) > 2)] # remove distant stars concentrated near zero proper motion, not equally distributed throughout patch
+    df = df[(df['b-r'] >= 0.5) & (df['b-r'] <=1 )] # isolates old and low-metallicity stellar streams in color space
+
+
+def get_results(df, top_n=250, save_folder='../results/simulated-streams/sim-1patch/plots'):
+    os.makedirs(save_folder, exist_ok=True)
+    
+    df_signalreg = df[df['region_label']==1]
+    df_backgroundreg = df[df['region_label']==0]
+
+    df_streamstar = df[df['stream']==True]
+    df_backgroundstar = df[df['stream']==False]
+
+    # NN scores
+    plt.hist(df_signalreg['nn_score'], label='Signal Region', histtype='step', bins=50)
+    plt.hist(df_backgroundreg['nn_score'], label='Background Region', histtype='step', bins=50)
+    plt.xlabel('NN Score')
+    plt.ylabel('Number of Stars')
+    plt.legend()
+    plt.show()
+    plt.savefig(os.path.join(save_folder, "nnscoreregions.png"))
+
+    plt.hist(df_streamstar['nn_score'], label='Stream Stars', color='red', histtype='step', bins=50)
+    plt.hist(df_backgroundstar['nn_score'], label='Background Stars', color='grey', histtype='step', bins=50)
+    plt.yscale('log')
+    plt.xlabel('NN Score')
+    plt.ylabel('Number of Stars')
+    plt.legend()
+    plt.show()
+    plt.savefig(os.path.join(save_folder, "nnscorestars.png"))
+
+    # top N stars
+    df_ranked = df.sort_values(by='nn_score', ascending=False)
+    df_top = df_ranked[:top_n]
+
+    # purity
+    purity = len(df_top[df_top['stream']==True]) / len(df_top)
+    print(f'Top {top_n} ranked stars: Purity = {purity*100}%')
+    
+    plt.scatter(df_streamstar['ra'], df_streamstar['dec'], label='GD-1 Stars', color='grey', marker='.', s=5)
+    plt.scatter(df_top['ra'], df_top['dec'], label=f'Top Ranked CWoLa Stars, purity = {purity*100}%', color='blue', marker='.', s=5)
+    df_top_stream = df_top[df_top['stream']==True]    
+    plt.scatter(df_top_stream['ra'], df_top_stream['dec'], label = 'Matches', color='red', marker='.', s=5)
+    plt.xlabel('Rotated Right Ascension ϕ [°]')
+    plt.ylabel('Rotated Declination λ [°]')
+    plt.legend()
+    plt.show()
+    plt.savefig(os.path.join(save_folder, "topnstars.png"))
+
+
+
 
 ### cleaning up and converting raw .npy data to .h5, functions from Dr. Pettee's repo https://github.com/hep-lbdl/GaiaCWoLa/tree/main
 
