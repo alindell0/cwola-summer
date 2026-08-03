e07bfba31166bc876defc9cda5df120613c38848
alindell0 <atlindell@gmail.com>
2026-08-03 10:10:31 -0500

    Refactor fullgd1.py to iterate over scan variables

--- scripts/fullgd1.py
+++ scripts/fullgd1.py
@@ -16,31 +16,36 @@ def train_on_patch(patch_idx, scan_var, fid_cuts):
     get_results(df_test_full, top_n=250, fid_cuts=fid_cuts, save_folder=f'../results/patch_{patch_idx}', patch_idx=patch_idx)
 
 
-## reproducibility with rotpmdec, ## comparison with rotpmra
+## reproducibility with rotpmdec and comparison with rotpmra
 
-### for gpu ???
-# rewrite idx = SLURM_ARRAY_TASK_ID 
-# array=0-20 in job submission
+scan_vars = ['rotpmdec', 'rotpmra']
 
-top_dfs = []
-gd1stars_dfs = []
-for idx in tqdm(range(21), desc='Patch'):
-  train_on_patch(patch_idx=idx, scan_var='rotpmra', fid_cuts=True)
+for i in range(len(scan_vars)):
+    scan_var = scan_vars[i]
 
-  df_test = pd.read_hdf(f'..results/training/patch_{idx}/df_test.h5')
-  df_gd1stars = df_test[df_test['stream']==True]
-  gd1stars_dfs.append(df_gd1stars)
-  
-  df_top = pd.read_hdf(f'../results/patch_{idx}/df_top.h5')
-  top_dfs.append(df_top)
+    top_dfs = []
+    gd1stars_dfs = []
+    for idx in tqdm(range(21), desc='Patch'):
+      train_on_patch(patch_idx=idx, scan_var=scan_var, fid_cuts=True)
+    
+      df_test = pd.read_hdf(f'..results/training/patch_{idx}/{scan_var}/df_test.h5')
+      df_gd1stars = df_test[df_test['stream']==True]
+      gd1stars_dfs.append(df_gd1stars)
+      
+      df_top = pd.read_hdf(f'../results/patch_{idx}/{scan_var}/df_top.h5')
+      top_dfs.append(df_top)
+    
+      print(f'Successfully loaded df_gd1 and df_top for Patch {idx} and scan variable {scan_var}')
+    
+    top_stars = pd.concat([df for df in top_dfs])
+    gd1_stars = pd.concat([df for df in gd1stars_dfs])
+    
+    top_stars = top_stars.drop_duplicates(ignore_index=True)
+    gd1_stars = gd1_stars.drop_duplicates(ignore_index=True)
 
-  print(f'Successfully loaded df_gd1 and df_top for Patch {idx}')
-
-
-top_stars = pd.concat([df for df in top_dfs])
-gd1_stars = pd.concat([df for df in gd1stars_dfs])
-
-# drop duplicates, plotting, etc
-
+# plotting, etc
 # compare final top stars non gd-1 after euclidean distance cutoff to those from paper? or reproduced with og pm param
+
+
+## comparison with rotpmra
 
