7855ca6d12a306845aa1b0e01dc6c2a0e24a024a
alindell0 <atlindell@gmail.com>
2026-08-03 09:51:49 -0500

    Fix syntax

--- scripts/fullgd1.py
+++ scripts/fullgd1.py
@@ -1,6 +1,6 @@
 from functions import *
 import pandas as pd
-import tqdm as tqdm
+from tqdm import tqdm
 
 ### full training function
 def train_on_patch(patch_idx, scan_var, fid_cuts):
@@ -10,7 +10,7 @@ def train_on_patch(patch_idx, scan_var, fid_cuts):
     plot_data(df, save_folder = f'../gaia-data/plots/patch_{patch_idx}')
     df_regions = signal_sideband(df, save_folder = f'../results/patch_{patch_idx}/{scan_var}', 
                                pm_parameter=scan_var, sig_factor=1, sb_factor=3)
-    df_test_full = cwola_train(df, pm_parameter=scan_var', dropout=0.2, k_folds=5, batch_size=10000, 
+    df_test_full = cwola_train(df, pm_parameter=scan_var, dropout=0.2, k_folds=5, batch_size=10000, 
                              lr=0.001, patience=30, epochs=100, trainval_loops=3, 
                              save_folder=f'../results/patch_{patch_idx}/training', wandbproj=None)
     get_results(df_test_full, top_n=250, fid_cuts=fid_cuts, save_folder=f'../results/patch_{patch_idx}', patch_idx=patch_idx)
