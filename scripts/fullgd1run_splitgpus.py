c374368d9570e9cac45f2ceeb9e668202ac03bcb
Anika Lindell <atlindell@gmail.com>
2026-08-11 15:12:45 -0700

    Condensed training scripts

--- scripts/fullgd1run_splitgpus.py
+++ scripts/fullgd1run_splitgpus.py
@@ -1,5 +1,6 @@
 import argparse
 
+# to submit with an sbatch array of 0-20, run patches on individual gpus in parallel
 parser = argparse.ArgumentParser()
 parser.add_argument("--patch", type=int, required=True,
                      help="Trains patch with given index (0-20)")
@@ -13,6 +14,6 @@ print(f"[patch {patch_id}] CUDA_VISIBLE_DEVICES = {os.environ.get('CUDA_VISIBLE_
 print(f"[patch {patch_id}] torch.cuda.device_count() = {torch.cuda.device_count()}")
 print(f"[patch {patch_id}] device name = {torch.cuda.get_device_name(0) if torch.cuda.is_available() else 'N/A'}")
 
-train_on_patch(patch_idx=patch_id, scan_var='rotpmdec', fid_cuts=True)
+train_on_patch(patch_idx=patch_id, scan_var='rotpmdec', fid_cuts=True) # scan_var = 'rotpmdec' or 'rotpmra'
 
 print('All done!')
--- scripts/patch0-rotpmra.py
+++
@@ -1,5 +0,0 @@
-from functions import *
-
-train_on_patch(patch_idx=0, scan_var='rotpmra', fid_cuts=True)
-
-print('All done!')
